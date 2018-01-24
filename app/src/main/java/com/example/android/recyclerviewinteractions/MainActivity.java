c7576fbf3548f7da4e2ac024757d0422b3166acf
Rob Hess <robwhess@gmail.com>
2018-01-23 16:21:14 -0800

    Add initial code.

---
+++ app/src/main/java/com/example/android/recyclerviewinteractions/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.recyclerviewinteractions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mTodoListRecyclerView;
+    private EditText mTodoEntryEditText;
+
+    private TodoAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTodoListRecyclerView = (RecyclerView)findViewById(R.id.rv_todo_list);
+
+        mTodoListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mTodoListRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new TodoAdapter();
+        mTodoListRecyclerView.setAdapter(mAdapter);
+
+        mTodoEntryEditText = (EditText)findViewById(R.id.et_todo_entry);
+
+        Button addTodoButton = (Button)findViewById(R.id.btn_add_todo);
+        addTodoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoText = mTodoEntryEditText.getText().toString();
+                if (!TextUtils.isEmpty(todoText)) {
+                    mAdapter.addTodo(todoText);
+                    mTodoEntryEditText.setText("");
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/android/recyclerviewinteractions/TodoAdapter.java
@@ -0,0 +1,61 @@
+package com.example.android.recyclerviewinteractions;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hessro on 1/18/18.
+ */
+
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
+
+    private ArrayList<String> mTodoList;
+
+    public TodoAdapter() {
+        mTodoList = new ArrayList<String>();
+    }
+
+    @Override
+    public TodoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.todo_list_item, parent, false);
+        TodoViewHolder viewHolder = new TodoViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(TodoViewHolder holder, int position) {
+        String todo = mTodoList.get(mTodoList.size() - position - 1);
+        holder.bind(todo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTodoList.size();
+    }
+
+    public void addTodo(String todo) {
+        mTodoList.add(todo);
+        notifyDataSetChanged();
+    }
+
+    class TodoViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTodoText;
+
+        public TodoViewHolder(View itemView) {
+            super(itemView);
+            mTodoText = (TextView)itemView.findViewById(R.id.tv_todo_text);
+        }
+
+        public void bind(String todo) {
+            mTodoText.setText(todo);
+        }
+    }
+
+}
